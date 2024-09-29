981214798d6846f955a9885a0d3a191cf911572e
Paul McKillop <pgmckillop@gmail.com>
2024-09-29 16:00:27 +0100

    2 Mwnu Option 2

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ def main_menu():
     print()
     # Options for the user to choose from
     print('1) Beginner: Hello basic')
+    print('2) Intermediate: Numbers Basic')
     print('99) Exit the program')
     # return the user's choice
     choice = int(input("\n  Enter a menu number: "))
@@ -33,10 +34,36 @@ create a Hello World script. It might seem trivial, but it's a very good test
 that the Input and Output features of the language are working.
 """
 
-
 def menu_option_1():
     forename = input("Enter your first name: ")
     print("Hello", forename)
+
+"""
+Menu Option 2. Intermediate: Numbers basic.
+Programming with a Sequence structure. 
+There is no conditional branching, and each
+instruction is carried out once only.
+"""
+
+
+def menu_option_2():
+    # set up an empty list to store the numbers entered
+    numbers_entered = []
+    print("You are going to be asked to enter 3 integer numbers. Enter them at the prompt.")
+    print(" ")
+    num1 = int(input("Enter the first number: "))
+    numbers_entered.append(num1)
+    num2 = int(input("Enter the second number: "))
+    numbers_entered.append(num2)
+    num3 = int(input("Enter the third number: "))
+    numbers_entered.append(num3)
+    print(" ")
+    print("The numbers you entered are: ", numbers_entered)
+
+
+
+
+
 
 """
 This is the application's menu implementation. It is called
@@ -50,12 +77,15 @@ It is set up to run until the user enters 99 to exit the structure.
 # Display menu when application launches
 x = main_menu()
 
-while x == 1 or x == 99:
+while x == 1 or x == 2 or x == 99:
     if x == 1:
         print(" ")
         print("You have selected the Beginner: Hello basic")
         # print("Hello World!")
         menu_option_1()
+    elif x == 2:
+        print("You have selected the Intermediate: Numbers Basic")
+        menu_option_2()
     elif x == 99:
         print(" ")
         print("You have selected to Exit the program")
